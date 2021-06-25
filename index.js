94b605af5d470a8b8f1eb845137bd5ecf83fa29a
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-06-25 18:50:03 +0600

    feat: Add listing by filename

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const fs = require('fs').promises
 
 const lsCommand = process.argv[2]
+const fileName = process.argv[3]
 
 async function main () {
   if (lsCommand === 'ls') {
     try {
-      // List all files in the current directory
       const files = await fs.readdir('./')
-      for (const file of files) {
-        console.log(file)
+      for (let i = 0; i < files.length; i++) {
+        // Show matching file name or an error if file does not exist
+        if (fileName) {
+          if (files[i] === fileName) {
+            return console.log(files[i])
+          } else if (i === files.length - 1) {
+            console.log('File name does not exist')
+          }
+        } else {
+          console.log(files[i])
+        }
       }
     } catch (err) {
       console.error(err)
     }
   } else {
-    console.log('Usage: node index.js ls')
+    console.log('Usage: node index.js ls filename')
   }
 }
 
