c07c49182da84b2ad11fe46cd382f3505713e79c
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-06-30 17:26:34 +0600

    feat: Show matching name for multiple files

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const fs = require('fs').promises
 
 const lsCommand = process.argv[2]
-const fileName = process.argv[3]
+const fileNames = process.argv.slice(3) 
 
 async function main () {
   if (lsCommand === 'ls') {
     try {
       // List all files in the current directory
-      if (!fileName) {
+      if (!fileNames.length) {
         const files = await fs.readdir('./')
         for (const file of files) {
           console.log(file)
         }
       }
-      // Show matching file name or an error if file does not exist
-      if (fileName) {
-        await fs.stat(`./${fileName}`)
-        console.log(fileName)
+      // Show matching file name for multiple files or error message if file
+      // does not exist
+      try {
+        if (fileNames.length) {
+          for (const i in fileNames) {
+            await fs.stat(fileNames[i])
+            console.log(fileNames[i])
+          }
+        }
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+          console.log('File or folder does not exist')
+        }
       }
     } catch (err) {
-      if (err.code === 'ENOENT') {
-        console.log('File or folder does not exist')
-      } else {
-        console.error(err)
-      }
+      console.error(err)
     }
   } else {
     console.log('Usage: node index.js ls filename')
