f189e3c2dca5f33f3ef4e9e9cea809320ffa5b56
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-07-01 17:01:11 +0600

    feat: Add ls directory

--- index.js
+++ index.js
@@ -13,15 +13,23 @@ async function main () {
           console.log(file)
         }
       }
-      // Show matching file name for multiple files or error message if file
-      // does not exist
-      for (const i in fileNames) {
-        try {
-          await fs.stat(fileNames[i])
-          console.log(fileNames[i])
-        } catch (err) {
-          if (err.code === 'ENOENT') {
-            console.log(`'${fileNames[i]}' does not exist`)
+      // List all files in dirname
+      if (fileNames[0].match(/\/.+\/.+/g)) {
+        const files = await fs.readdir(fileNames[0])
+        for (const file of files) {
+          console.log(file)
+        }
+      } else {
+        // Show matching file name for multiple files or error message if file
+        // does not exist
+        for (const i in fileNames) {
+          try {
+            await fs.stat(fileNames[i])
+            console.log(fileNames[i])
+          } catch (err) {
+            if (err.code === 'ENOENT') {
+              console.log(`'${fileNames[i]}' does not exist`)
+            }
           }
         }
       }
