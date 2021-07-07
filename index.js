901f821540cca1fedf432840ba73227c333ff291
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-07-07 16:05:09 +0600

    feat: Add listing for directory

--- index.js
+++ index.js
@@ -17,8 +17,16 @@ async function main () {
       // does not exist
       for (const file of fileNames) {
         try {
-          await fs.stat(file)
-          console.log(file)
+          const fileStat = await fs.stat(file)
+          // List files inside a directory
+          if (fileStat.isDirectory()) {
+            const dirFiles = await fs.readdir(file)
+            for (const file of dirFiles) {
+              console.log(file)
+            }
+          } else {
+            console.log(file)
+          }
         } catch (err) {
           if (err.code === 'ENOENT') {
             console.log(`'${file}' does not exist`)
